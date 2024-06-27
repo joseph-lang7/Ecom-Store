af39a22291f8c887a87bbb099ecb99b618b90d71
joseph <langj6757@gmail.com>
2024-06-26 19:01:07 -0700

    Make benefit banner responsive

--- app/components/benefits-banner/benefits-banner.tsx
+++ app/components/benefits-banner/benefits-banner.tsx
@@ -7,27 +7,31 @@ import { BenefitElement } from "./components/benefit-element";
 
 export const BenefitsBanner = () => {
   return (
-    <div className="bg-[#e0dfdf] py-7 flex flex-col gap-10">
-      <BenefitElement
-        icon={<FaTruck />}
-        title="Free Shipping"
-        description="Free Shipping on all orders over $99"
-      />
-      <BenefitElement
-        icon={<FaHandHoldingDollar />}
-        title="Competitive Pricing"
-        description="Get the best deals"
-      />
-      <BenefitElement
-        icon={<FaGift />}
-        title="Free Gift"
-        description="Free Gift with every order"
-      />
-      <BenefitElement
-        icon={<FaRegClock />}
-        title="Open All Week"
-        description="9:00am - 9:00pm"
-      />
+    <div className="my-[120px]">
+      <div className="flex justify-center w-full bg-[#e0dfdf]">
+        <div className="py-7 flex flex-col lg:flex-row gap-10 xl:gap-16 px-5">
+          <BenefitElement
+            icon={<FaTruck />}
+            title="Free Shipping"
+            description="Free Shipping on all orders over $99"
+          />
+          <BenefitElement
+            icon={<FaHandHoldingDollar />}
+            title="Competitive Pricing"
+            description="Get the best deals"
+          />
+          <BenefitElement
+            icon={<FaGift />}
+            title="Free Gift"
+            description="Free Gift with every order"
+          />
+          <BenefitElement
+            icon={<FaRegClock />}
+            title="Open All Week"
+            description="9:00am - 9:00pm"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- app/components/benefits-banner/components/benefit-element.tsx
+++ app/components/benefits-banner/components/benefit-element.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const BenefitElement = ({ icon, title, description }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-1">
+    <div className="flex flex-col items-center justify-center gap-1 text-center">
       <div className="text-red-700 text-2xl">{icon}</div>
       <div>{title}</div>
       <div className="text-xs text-gray-500">{description}</div>
